212026fa1cec574291a30f7e9428d3c3f50890d3
ip320-15 <niikolajkirillov777@gmail.com>
2023-11-27 15:58:15 +0300

    Создание файла

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Git_Bash_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My fist git-repo");
+
+        }
+    }
+}
